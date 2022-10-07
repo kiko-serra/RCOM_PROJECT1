7c7d189f3124e6f3065d0f6b7a8b4c9b7eb2f45f
aroozcozido <up202004293@up.pt>
2022-10-07 11:01:17 +0100

    minor changes in the config

--- src/write_noncanonical.c
+++ src/write_noncanonical.c
@@ -78,7 +78,7 @@ int main(int argc, char *argv[])
 
     // Set input mode (non-canonical, no echo,...)
     newtio.c_lflag = 0;
-    newtio.c_cc[VTIME] = 30; // Inter-character timer unused
+    newtio.c_cc[VTIME] = 10; // Inter-character timer unused
     newtio.c_cc[VMIN] = 0;  // Blocking read until 5 chars received
 
     // VTIME e VMIN should be changed in order to protect with a
