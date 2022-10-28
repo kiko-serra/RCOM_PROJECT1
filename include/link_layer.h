b11fb8a78bed89e0c9b9ae0ea961ec8c8b63b1e6
Francisco Maria Lopes Pinto Pimentel Serra <up202007723@fe.up.pt>
2022-10-28 10:00:41 +0000

    Update link_layer.h

--- include/link_layer.h
+++ include/link_layer.h
@@ -4,6 +4,8 @@
 #ifndef _LINK_LAYER_H_
 #define _LINK_LAYER_H_
 
+#include "constants.h"
+
 typedef enum
 {
     LlTx,
@@ -19,13 +21,6 @@ typedef struct
     int timeout;
 } LinkLayer;
 
-// SIZE of maximum acceptable payload.
-// Maximum number of bytes that application layer should send to link layer
-#define MAX_PAYLOAD_SIZE 1000
-
-// MISC
-#define FALSE 0
-#define TRUE 1
 
 // Open a connection using the "port" parameters defined in struct linkLayer.
 // Return "1" on success or "-1" on error.
